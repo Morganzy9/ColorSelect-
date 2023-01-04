059877b8335c77989fe47a7ca497de19c7131c8e
islombek <islombek.pulatov9@gmail.com>
2023-01-04 15:44:50 +0500

    Initial Commit

---
+++ ColorSelect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSelect
+//
+//  Created by Ислам Пулатов on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
